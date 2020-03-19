88fa378c9bc78e3815804df927840705f2f8d0ab
ATM john <john.botella@atm-consulting.fr>
2020-03-19 14:27:29 +0100

    Add page event from module builder :-)

--- lib/operationorder.lib.php
+++ lib/operationorder.lib.php
@@ -139,6 +139,17 @@ function operationorder_prepare_head(OperationOrder $object)
     $head[$h][2] = 'document';
     $h++;
 
+    $head[$h][0] = dol_buildpath("/operationorder/operationorder_agenda.php", 1).'?id='.$object->id;
+    $head[$h][1] = $langs->trans("Events");
+    $head[$h][2] = 'agenda';
+    $h++;
+
+    $head[$h][0] = dol_buildpath("/operationorder/operationorder_info.php", 1).'?id='.$object->id;
+    $head[$h][1] = $langs->trans("OperationOrderInfo");
+    $head[$h][2] = 'info';
+    $h++;
+
+
     // Show more tabs from modules
     // Entries must be declared in modules descriptor with line
     //$this->tabs = array(
