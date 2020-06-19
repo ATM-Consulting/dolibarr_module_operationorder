4f18e815ae419f32674b3736b599bf48065f05cb
atm-greg <gregory.blemand@atm-consulting.fr>
2020-06-19 10:48:53 +0200

    add menu

--- core/modules/modOperationOrder.class.php
+++ core/modules/modOperationOrder.class.php
@@ -393,6 +393,22 @@ class modOperationOrder extends DolibarrModules
         );				                // 0=Menu for internal users, 1=external users, 2=both
         $r++;
 
+        $this->menu[$r]=array(
+            'fk_menu'=>'fk_mainmenu=operationorder,fk_leftmenu=operationorder_left',		    // Use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
+            'type'=>'left',			                // This is a Left menu entry
+            'titre'=>$langs->trans('LeftMenuOperationOrderManager'),
+            'mainmenu'=>'operationorder',
+            'leftmenu'=>'operationorder_left_manager',
+            'url'=>'/operationorder/operationorder_manager.php',
+            'langs'=>'operationorder@operationorder',	        // Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
+            'position'=>1000+$r,
+            'enabled'=> '$conf->operationorder->enabled',  // Define condition to show or hide menu entry. Use '$conf->missionorder->enabled' if entry must be visible if module is enabled. Use '$leftmenu==\'system\'' to show if leftmenu system is selected.
+            'perms'=> '$user->rights->operationorder->manager->read',			                // Use 'perms'=>'$user->rights->missionorder->level1->level2' if you want your menu with a permission rules
+            'target'=>'',
+            'user'=>0
+        );				                // 0=Menu for internal users, 1=external users, 2=both
+        $r++;
+
 
 
 		$this->menu[$r]=array(
